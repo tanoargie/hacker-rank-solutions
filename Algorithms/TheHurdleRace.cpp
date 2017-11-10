bbfddf81ab6dfbe8d137d557291985f9011f686a
franco <francoagustinserio@gmail.com>
2017-11-10 09:02:42 -0300

    The Hurdle Race

---
+++ Algorithms/TheHurdleRace.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    int max = *max_element(height.begin(),height.end());
+    if ((max - k) > 0) {
+        cout << max - k << endl;
+    } else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
+
