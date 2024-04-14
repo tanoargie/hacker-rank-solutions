ec2dd6994261fa1d62043218dee1c55afb0f9417
Franco Serio <franco@samser.co>
2024-04-14 11:35:30 +0200

    feat: PreOrderTraversal

---
+++ DataStructures/TreePreorderTraversal.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+  Node *left, *right;
+  int data;
+
+  Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+class Tree {
+public:
+  Node *insert(Node *root, int data) {
+    if (root == nullptr) {
+      return new Node(data);
+    } else {
+      Node *cur;
+      if (data <= root->data) {
+        cur = insert(root->left, data);
+        root->left = cur;
+      } else {
+        cur = insert(root->right, data);
+        root->right = cur;
+      }
+
+      return root;
+    }
+  }
+  void preOrder(Node *root) {
+    if (root == nullptr) {
+      return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+  }
+};
+
+int main() {
+  Tree tree;
+  int numberOfNodes;
+  Node *root;
+  cin >> numberOfNodes;
+  int val;
+  for (int i = 0; i < numberOfNodes; i++) {
+    cin >> val;
+    if (i == 0) {
+      root = tree.insert(NULL, val);
+    } else {
+      tree.insert(root, val);
+    }
+  }
+
+  tree.preOrder(root);
+  cout << endl;
+
+  return 0;
+}
